b274aea80009207677faaf4a1967e12feae2eee1
Fajri Rinaldi Chan <fajri@gariskode.com>
2024-11-18 14:26:17 +0700

    update

--- resources/views/back/layout/header/__topbar.blade.php
+++ resources/views/back/layout/header/__topbar.blade.php
@@ -1,18 +1,18 @@
 <!--begin::Toolbar wrapper-->
 <div class="d-flex flex-shrink-0">
     <!--begin::Invite user-->
-    <div class="d-flex ms-3">
+    {{-- <div class="d-flex ms-3">
         <a href="#" class="btn btn-flex flex-center btn-light btn-color-gray-600 btn-active-primary w-40px w-md-auto h-40px px-0 px-md-6"  data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends" >
             <i class="ki-duotone ki-plus fs-2 me-0 me-md-2"></i>            <span class="d-none d-md-inline">New Member</span>
         </a>
-    </div>
+    </div> --}}
     <!--end::Invite user-->
     <!--begin::Create app-->
-    <div class="d-flex ms-3">
+    {{-- <div class="d-flex ms-3">
         <a href="#" class="btn btn-flex btn-light flex-center btn-color-gray-600 btn-active-primary w-40px w-md-auto h-40px px-0 px-md-6"  data-bs-toggle="modal" data-bs-target="#kt_modal_create_app" id="kt_toolbar_primary_button">
             <i class="ki-duotone ki-document fs-2 me-0 me-md-2"><span class="path1"></span><span class="path2"></span></i>            <span class="d-none d-md-inline">New App</span>
         </a>
-    </div>
+    </div> --}}
     <!--end::Create app-->
     <!--begin::Theme mode-->
     <div class="d-flex align-items-center ms-3">
