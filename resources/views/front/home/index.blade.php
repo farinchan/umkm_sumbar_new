ec82ca0ca3f5072b54785cefc08f818bacdef14d
Fajri Rinaldi Chan <fajri@gariskode.com>
2024-11-18 14:15:29 +0700

    update

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -176,14 +176,10 @@
             <div class="container margin_60">
                 <div class="row justify-content-center justify-content-md-start">
                     <div class="col-lg-6 wow" data-wow-offset="150">
-                        <h3>Armor<br>Air Color 720</h3>
-                        <p>Lightweight cushioning and durable support with a Phylon midsole</p>
+                        <h3>Smart-UMKM <br>Sumatera Barat</h3>
+                        <p> {{ Str::limit(strip_tags($website->about), 200) }}</p>
                         <div class="feat_text_block">
-                            <div class="price_box">
-                                <span class="new_price">$90.00</span>
-                                <span class="old_price">$170.00</span>
-                            </div>
-                            <a class="btn_1" href="listing-grid-1-full.html" role="button">Shop Now</a>
+                            <a class="btn_1" href="{{ route("product-all") }}" role="button">Belanja Sekarang</a>
                         </div>
                     </div>
                 </div>
